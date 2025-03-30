689f8a114b8b3f280c76769da58d03a468df7675
fulleni <fulleni@hotmail.com>
2025-03-30 11:25:28 +0100

    build: initial project setup
    - Added issue templates
    - Added pull request template
    - Added basic CI workflow

---
+++ lib/src/ht_sources_repository.dart
@@ -0,0 +1,7 @@
+/// {@template ht_sources_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtSourcesRepository {
+  /// {@macro ht_sources_repository}
+  const HtSourcesRepository();
+}
